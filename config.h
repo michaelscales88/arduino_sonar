989f80d9a0f8449b0e848f5a9361c0cc4ac3537d
Michael Scales <mscales@mindwireless.com>
2017-05-03 17:08:24 -0500

    laying out some parts

--- config.h
+++ config.h
@@ -3,21 +3,17 @@
 #include <avr/io.h>
 #include "../include/AVRSpecialRegs.inc"
 
-#define LED_DIR     _DDRB
-#define LED_PORT    _PORTB
-#define LED_PIN     7
+#define LED_DIR         _DDRB
+#define LED_PORT        _PORTB
+#define LED_PIN         7
 
-#define TRIGGER_DIR    _DDRD
-#define TRIGGER_PORT   _PORTD
-#define TRIGGER_PIN    1
+#define TRIGGER_DIR     _DDRD
+#define TRIGGER_PORT    _PORTD
+#define TRIGGER_PIN     1
 
-#define PIND     0x09
+#define ECHO_DIR        _DDRD
+#define ECHO_PORT       _PORTD
+#define ECHO_PIN        2               ; echo pin
 
-#define ECHO        2
-#define DELAY       100
-
-; #define PIND        0x09			; the input PIND register
-;#define LED_PIN     5			    ; Define LED pin on PORTB
-; #define	TRIGGER     1			; Trigger Pin
-; #define ECHO        2			; Echo Pin
-; #define DELAY       100			; Delay constant
+#define PIND            0x09            ; the input PIND register
+#define DELAY           100             ; delay constant

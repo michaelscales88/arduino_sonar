076697ccfd3dd201ddd72f0391a68432d1f56cd4
Michael Scales <mscales@mindwireless.com>
2017-05-05 18:19:42 -0500

    setting up skeleton

--- config.h
+++ config.h
@@ -7,13 +7,9 @@
 #define LED_PORT        _PORTB
 #define LED_PIN         7
 
-#define TRIGGER_DIR     _DDRD
-#define TRIGGER_PORT    _PORTD
+#define SONAR_DIR       _DDRD
+#define SONAR_PORT      _PORTD
 #define TRIGGER_PIN     1
-
-#define ECHO_DIR        _DDRD
-#define ECHO_PORT       _PORTD
 #define ECHO_PIN        2               ; echo pin
 
-#define PIND            0x09            ; the input PIND register
 #define DELAY           100             ; delay constant
